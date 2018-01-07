975923f0e27958c008f1f9c2bb38ad61ecaeb38a
Rishabh-Rj <rishabh_joshi@srmuniv.edu.in>
2018-01-07 20:50:01 +0530

    Initial commit

---
+++ text analyser.py
@@ -0,0 +1,39 @@
+def count_char(text, char):
+    count = 0
+    for c in text:
+        if c == char:
+            count = count + 1
+
+    return count
+
+
+file = open("newfile.txt", "w")
+file.write("""
+The club isn't the best place to find a lover
+So the bar is where I go
+Me and my friends at the table doing shots
+Drinking fast and then we talk slow
+Come over and start up a conversation with just me
+And trust me I'll give it a chance now
+Take my hand, stop, put Van the Man on the jukebox
+And then we start to dance, and now I'm singing like
+Girl, you know I want your love
+Your love was handmade for somebody like me
+Come on now, follow my lead
+I may be crazy, don't mind me
+Say, boy, let's not talk too much
+Grab on my waist and put that body on me
+Come on now, follow my lead
+Come, come on now, follow my lead
+I'm in love with the shape of you
+We push and pull like a magnet do
+Although my heart is falling too
+I'm in love with your body """)
+file.close()
+filename = "newfile.txt"
+with open(filename) as f:
+    text = f.read()
+
+for char in "abcdefghijklmnopqrstuvwxyz":
+    perc = 100 * count_char(text, char) / len(text)
+    print("{0} - {1}%".format(char, round(perc, 2)))
